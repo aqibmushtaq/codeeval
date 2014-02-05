240c680ddc7c9e0d2eaf130184c2a25ad1c8928d
Aqib Mushtaq <aqib@mushtaq.me>
2014-02-05 00:15:08 +0000

    Implemented non contiguous longest common sequence code

--- longest-common-sequence/LongestCommonSequence.java
+++ longest-common-sequence/LongestCommonSequence.java
@@ -1,35 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class LongestCommonSequence {
 
-    private static String calcLongestContinousSequence (String str1, String str2) {
-        int size = 1;
-        int start = 0;
-        String longest = "";
-        while (size != str1.length()) {
-            System.out.printf("size: %d\tstart: %d\n", size, start);
-            String str = str1.substring(start, start+size);
-            if (str2.contains(str)) {
-                size++;
-                start = 0;
-                longest = str;
-                continue;
-            }
-
-            start++;
-
-            if (start + size > str1.length()) {
-                size++;
-                start = 0;
-                continue;
-            }
+    private static Node<Character> buildGraph (String str) {
+        if (str.length() == 1) {
+            return new Node<Character>(str.charAt(0));
         }
 
-        return longest;
+        //create this node
+        Node<Character> thisNode = new Node<Character>(str.charAt(0));
+
+        //create node for each character
+        List<Node<Character>> nodes = new ArrayList<Node<Character>>();
+        thisNode.next = nodes;
+        for (int i = 1; i < str.length(); i++) {
+            nodes.add(buildGraph(str.substring(i)));
+        }
+
+        return thisNode;
     }
-    
+
+    private static Set<String> getAllCombinations (Node<Character> root) {
+        Set<String> combs = new HashSet<String>();
+        if (root.next == null) {
+            combs.add(root.data.toString());
+            return combs;
+        }
+
+        combs.add(root.data.toString());
+        for (int i = 0; i < root.next.size(); i++) {
+            combs.add(root.data.toString() + root.next.get(i).data.toString());
+            Set<String> combs2 = getAllCombinations(root.next.get(i));
+            Iterator<String> combs2i = combs2.iterator();
+            while (combs2i.hasNext()) {
+                String str = combs2i.next();
+                combs.add(root.data.toString() + str);
+                combs.add(str);
+            }
+        }
+        return combs;
+    }
+
     public static void main (String[] args) {
         String[] input = new String[2];
 
@@ -41,19 +55,45 @@ public class LongestCommonSequence {
             System.exit(1);
         }
 
-        if (input.length != 2 || input[0].length() == 0 || input[1].length() == 0) {
-            System.out.printf("Input is invalid, array size = %d, length of str1 = %d, str2 = %d", input.length, input[0].length(), input[1].length());
-            System.exit(1);
+        Node<Character> n1 = buildGraph(input[0]);
+        Node<Character> n2 = buildGraph(input[1]);
+
+        Set<String> set1 = getAllCombinations(n1);
+        Set<String> set2 = getAllCombinations(n2);
+
+        set1.retainAll(set2);
+
+        String longestSoFar = "";
+        int max = -1;
+        for (String s : set1) {
+            int l = s.length();
+            if(l > max){
+                max = l;
+                longestSoFar = s;
+            }
         }
+        System.out.println(longestSoFar);
+    }
 
-        //get longest common sequence
-        String longest = "";
-        if (input[0].length() < input[1].length())
-            longest = calcLongestContinousSequence(input[0], input[1]);
-        else 
-            longest = calcLongestContinousSequence(input[1], input[0]);
+    private static class Node<T> {
+        public T data;
+        public List<Node<T>> next;
 
-        System.out.println(longest);
+        public Node (T data) {
+            this.data = data;
+        }
+        public Node (T data, List<Node<T>> next) {
+            this.data = data;
+            this.next = next;
+        }
+        @Override
+        public String toString() {
+            String next = "";
+            for (int i = 0; i < next.length(); i++) {
+                next += next.toString() + " ";
+            }
+            return data.toString() + " -> " + next;
+        }
     }
 
 }
